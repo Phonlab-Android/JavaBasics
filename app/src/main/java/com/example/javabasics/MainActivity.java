a168adaec3b431803d1f28ec8d45dce621e861df
Michael Highsmith <Phonlabtech@gmail.com>
2020-01-26 13:24:53 -0600

    Data Types

--- app/src/main/java/com/example/javabasics/MainActivity.java
+++ app/src/main/java/com/example/javabasics/MainActivity.java
@@ -19,16 +19,20 @@ public class MainActivity extends AppCompatActivity {
 
     //This is a method that is called when our button is clicked by the user
     public void clickButton(View view) {
-        int x = 3;
-        int y = 6;
-        int z = 9;
-        int anotherVariable = x - y - z;
-        int remainderVariable = z % x;
-        updateTextView(remainderVariable);
+        byte byteVar = 5;
+        short shortVar = 16;
+        int intVar = 888;
+        long longVar = 5000000L;
+        float floatVar = 50.0f;
+        double doubleVar = 412.6;
+        char charVar = 'a';
+        boolean booleanVar = true;
+        String stringVar = "My first string";
+        updateTextView(stringVar);
     }
 
     //This method will update our TextView with the id "out_first_text_view"
-    public void updateTextView(int number) {
+    public void updateTextView(String number) {
         TextView ourFirstTextView = findViewById(R.id.our_first_text_view);
         ourFirstTextView.setText("" + number);
     }
